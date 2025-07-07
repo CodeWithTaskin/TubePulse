38a5fbf3db96e365d31a5c7f69e7ef02f6b3e6f1
Farjhan Ahmed <nkftt58346@gmail.com>
2025-07-07 18:47:09 +0600

    feat: add MongoDB connection class with error handling and logging

---
+++ src/configuration/mongobd/mongo_db_connection.py
@@ -0,0 +1,26 @@
+import sys
+from pymongo import MongoClient
+
+from src.logging import logging
+from src.exception import MyException
+
+
+class MongoDBConnection:
+    def __init__(self, db_name: str, collection_name: str, connection_url: str):
+        try:
+            client = MongoClient(connection_url)
+            status = client.admin.command('ping')
+            if status['ok'] == 1:
+                logging.info('MongoDB Connected Successfully....')
+                database = client[db_name]
+                self.collection = database[collection_name]
+            else:
+                logging.info('MongoDB Connection Failed....')
+        except Exception as e:
+            raise MyException(e, sys) from e
+    
+    def collection_entity(self):
+        try:
+            return self.collection
+        except Exception as e:
+            raise MyException(e, sys) from e
