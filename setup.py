adfe29510bd55810a3fd9c1d6161400107d79912
Farjhan Ahmed <nkftt58346@gmail.com>
2025-07-07 09:51:34 +0600

    fix: revert version number to 0.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="src",
-    version="0.0.2",
+    version="0.0.1",
     author="Farjhan Ahmed",
     author_email="farjhanahmed@gmail.com",
     packages=find_packages()
